baf107295e0eb08fbbae2a613b3a0c319bec859a
John Doe <johndoe@on.net>
2026-04-08 13:40:17 +0900

    Suspense

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import NavbarItem from "./NavbarItem";
+import { Suspense } from "react";
 export default function Navbar() {
   return (
-    <div className="flex dark:bg-gray-600 bg-gray-100 p-4 lg:text-lg items-center justify-center gap-4">
-      <NavbarItem title="Trending" param="fetchTrending"/>
-      <NavbarItem title="Top Rated" param="fetchTopRated"/>
-    </div>
+    <Suspense fallback={null}>
+      <div className="flex dark:bg-gray-600 bg-gray-100 p-4 lg:text-lg items-center justify-center gap-4">
+        <NavbarItem title="Trending" param="fetchTrending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </div>
+    </Suspense>
   );
 }
